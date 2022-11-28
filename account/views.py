bc7174f74e597c898803d353d3aee0714bb4b578
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-28 08:46:59 +0100

    add verify view

--- account/views.py
+++ account/views.py
@@ -1,10 +1,10 @@
-# Create your views here.
+import uuid
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 from .forms import SignUpForm
-from .models import Customer
+from .models import Customer, Verify
 
 User = get_user_model()
 
@@ -32,6 +32,8 @@ def register_view(request):
                 customer = Customer.objects.create(
                     user=user, first_name=f_name, last_name=l_name, mobile=phone)
                 customer.save()
+                obj = Verify.objects.create(user=user, token=uuid.uuid4())
+                obj.save()
                 return redirect('/')
 
     else:
@@ -39,3 +41,13 @@ def register_view(request):
 
     context = {'signup_form': signup_form}
     return render(request, "account/registration/register.html", context)
+
+
+def verify_user(request, token):
+    obj = Verify.objects.get(token=token)
+    user = obj.user
+    user.is_active = True  # change status to active to enable login
+    user.save()
+    del obj  # delete verify_instance after successful verification
+    # return redirect(reverse('account:login'))  #to be implement
+    return redirect('/')
